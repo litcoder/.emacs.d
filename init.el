9f27ba9c8c20eea44f5b736b935279ba17336826
litcoder <brandon.hong@intel.com>
2018-11-08 13:21:12 +0800

    Fixed initialization error
    Signed-off-by: litcoder <brandon.hong@intel.com>

--- init.el
+++ init.el
@@ -8,6 +8,28 @@
 
 
 ;;; **************************************************************
+;;; Package install
+;;; **************************************************************
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+
+;;; **************************************************************
 ;;; Code style
 ;;;  From: https://www.emacswiki.org/emacs/IndentingC
 ;;;    A partial list of the better known C styles:
@@ -93,41 +115,41 @@
 ;;; **************************************************************
 ;;; Git
 ;;; **************************************************************
-(require 'git)
+;; (require 'git)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode elpy magit git))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-;; Bind magit status
-(global-set-key (kbd "C-x g") 'magit-status)
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages (quote (markdown-mode elpy magit git))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+;; ;; Bind magit status
+;; (global-set-key (kbd "C-x g") 'magit-status)
 
 
 
 ;;; **************************************************************
 ;;; Elpy
 ;;; **************************************************************
-(elpy-enable)
+;;(elpy-enable)
 ;; CPython
 ;;(setq python-shell-interpreter "python"
 ;;      python-shell-interpreter-args "-i")
 ;; IPython
-(setq python-shell-interpreter "ipython"
-        python-shell-interpreter-args "--simple-prompt -i")
+;;(setq python-shell-interpreter "ipython"
+;;        python-shell-interpreter-args "--simple-prompt -i")
 
 ;;; **************************************************************
 ;;; CTags - https://www.emacswiki.org/emacs/BuildTags
 ;;; **************************************************************
 (setq path-to-ctags "/usr/bin/ctags")
-(defuc create-tags (dir-name)
+(defun create-tags (dir-name)
   "Create tags file."
   (interactive "DDirectory: ")
   (shell-command
