2e8d2690677d1be1662d0a0024736992ce35c9cf
Brandon Hong <brandon.hong@intel.com>
2019-08-26 08:45:23 +0800

    Fixed with font Courier
    Signed-off-by: Brandon Hong <brandon.hong@intel.com>

--- init.el
+++ init.el
@@ -166,3 +166,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;;; **************************************************************
+;;; Font - Courier
+;;; **************************************************************
+(set-frame-font "Courier 12" nil t)
