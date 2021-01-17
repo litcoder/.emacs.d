080ce810ee7f501956947e1a3d3056591e3282e9
litcoder <litcoderwiz@gmail.com>
2021-01-17 18:14:15 +0900

    Theme misterioso
    Signed-off-by: litcoder <litcoderwiz@gmail.com>

--- init.el
+++ init.el
@@ -167,7 +167,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (plantuml-mode git go-mode tramp-theme gited))))
+ '(custom-enabled-themes '(misterioso))
+ '(package-selected-packages '(plantuml-mode git go-mode tramp-theme gited)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
