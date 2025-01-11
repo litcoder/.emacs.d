c05485ba9de328d0a2aab6740de6915efc013a8d
litcoder <litcoderwiz@gmail.com>
2025-01-11 22:20:44 +0900

    Inkpot theme
    Signed-off-by: litcoder <litcoderwiz@gmail.com>

--- init.el
+++ init.el
@@ -40,6 +40,7 @@
         lsp-mode
         lsp-ui
         rust-mode
+        inkpot-theme
         s
         dash
         editorconfig
@@ -162,6 +163,12 @@
   :diminish)
 
 
+;;; **************************************************************
+;;; Theme
+;;; **************************************************************
+(load-theme 'inkpot t)
+
+
 
 ;;; **************************************************************
 ;;; Spelling checker. e.g) M-x flyspell-mode
@@ -170,16 +177,3 @@
 ;;; **************************************************************
 (setq ispell-program-name "/usr/local/bin/aspell")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(use-package lsp-ui rust-mode python-black py-autopep8 magit lsp-mode find-file-in-project exec-path-from-shell elpy clang-format auto-complete)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
